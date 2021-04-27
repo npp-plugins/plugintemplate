bde87a13c94f3316026b131eee7063feca48817d
Don HO <don.h@free.fr>
2021-04-27 14:55:25 +0200

    Prevent plugin from crash

--- src/NppPluginDemo.cpp
+++ src/NppPluginDemo.cpp
@@ -23,22 +23,26 @@ extern NppData nppData;
 
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD  reasonForCall, LPVOID /*lpReserved*/)
 {
-    switch (reasonForCall)
-    {
-      case DLL_PROCESS_ATTACH:
-        pluginInit(hModule);
-        break;
+	try {
 
-      case DLL_PROCESS_DETACH:
-        pluginCleanUp();
-        break;
+		switch (reasonForCall)
+		{
+			case DLL_PROCESS_ATTACH:
+				pluginInit(hModule);
+				break;
 
-      case DLL_THREAD_ATTACH:
-        break;
+			case DLL_PROCESS_DETACH:
+				pluginCleanUp();
+				break;
 
-      case DLL_THREAD_DETACH:
-        break;
-    }
+			case DLL_THREAD_ATTACH:
+				break;
+
+			case DLL_THREAD_DETACH:
+				break;
+		}
+	}
+	catch (...) { return FALSE; }
 
     return TRUE;
 }
